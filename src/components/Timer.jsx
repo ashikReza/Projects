ad7c775f7e42d84142eeb1d376a91c81a8316577
Ashik044 <rezaashik044@gmail.com>
2024-02-01 17:38:41 +0600

    done

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -140,7 +140,7 @@ const Timer = () => {
             Short Break
           </button>
           <button
-            className="px-5 py-2 bg-[#00D991A1] rounded mt-4 text-white font-bold transform hover:scale-125 transition duration-300 ease-in-out"
+            className="px-5 py-2 bg-[#8FA6AC] rounded mt-4 text-white font-bold transform hover:scale-125 transition duration-300 ease-in-out"
             onClick={() => switchTimerType("longBreak")}
           >
             Long Break
