04decaa215a06fba4df39f565c78e292fc3e99a5
Ashik044 <rezaashik044@gmail.com>
2024-02-01 16:03:19 +0600

    done

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -12,12 +12,14 @@ const Timer = () => {
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
     setMinutes(getInitialMinutes(timerType));
     setSeconds(0);
     setIsActive(false);
     setTimerStarted(false);
+    setProgress(0); // Reset progress when timer type changes
   }, [timerType]);
 
   useEffect(() => {
@@ -27,6 +29,13 @@ const Timer = () => {
       setTimerStarted(true); // Set timerStarted to true when the timer is started
 
       interval = setInterval(() => {
+        const totalSeconds = minutes * 60 + seconds;
+        const elapsedSeconds = getInitialMinutes(timerType) * 60 - totalSeconds;
+
+        setProgress(
+          (elapsedSeconds / (getInitialMinutes(timerType) * 60)) * 100
+        );
+
         if (seconds === 0) {
           if (minutes === 0) {
             // Timer has reached 0
@@ -44,7 +53,7 @@ const Timer = () => {
     }
 
     return () => clearInterval(interval);
-  }, [isActive, minutes, seconds]);
+  }, [isActive, minutes, seconds, timerType]);
 
   const resetTimer = () => {
     // Play the click sound when the button is clicked
@@ -54,6 +63,7 @@ const Timer = () => {
     setSeconds(0);
     setIsActive(false);
     setTimerStarted(false); // Set timerStarted back to false when the timer is reset
+    setProgress(0); // Reset progress when timer is reset
   };
 
   // Define a function to play the click sound
@@ -80,28 +90,34 @@ const Timer = () => {
       )} h-screen flex items-center justify-center`}
     >
       <div
-        className="backdrop-blur-lg p-10 rounded"
+        className="backdrop-blur-lg p-10 rounded mx-4"
         style={{ backgroundColor: "rgba(225, 225, 255, 0.3)" }}
       >
-        <div className="flex justify-center mb-8">
-          <p className=" text-3xl font-extrabold">
+        <div
+          className="absolute top-0 left-0 bg-white h-1 rounded"
+          style={{ width: `${progress}%` }}
+        ></div>
+        <div className="flex justify-center ">
+          <p className="text-3xl font-extrabold">
             {getTimerTypeName(timerType)}
           </p>
         </div>
-        <div className="flex justify-center">
-          <p className=" text-4xl font-semibold">{`${minutes
-            .toString()
-            .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}</p>
+        <div className="flex justify-center relative my-5 sm:my-8">
+          <p className="text-4xl font-semibold">
+            {`${minutes.toString().padStart(2, "0")}:${seconds
+              .toString()
+              .padStart(2, "0")}`}
+          </p>
         </div>
         <div className="flex justify-center gap-4 ">
           <button
-            className="w-[5rem] py-2 bg-green-300 rounded mt-4  font-extrabold"
+            className="w-[5rem] py-2 bg-green-300 rounded font-extrabold"
             onClick={toggleTimer}
           >
             {isActive ? "Pause" : "Start"}
           </button>
           <button
-            className={`px-5 py-2 bg-green-300 rounded mt-4  font-extrabold ${
+            className={`px-5 py-2 bg-green-300 rounded font-extrabold ${
               timerStarted ? "" : "opacity-50 cursor-not-allowed"
             }`}
             onClick={resetTimer}
@@ -130,7 +146,6 @@ const Timer = () => {
             Long Break
           </button>
         </div>
-
         <div className="flex items-center justify-center mt-4 font-semibold ">
           {getFooterTypeName(timerType)}
         </div>
@@ -144,7 +159,7 @@ const getInitialMinutes = (timerType) => {
     case "pomodoro":
       return 25;
     case "shortBreak":
-      return 5;
+      return 1;
     case "longBreak":
       return 15;
     default:
