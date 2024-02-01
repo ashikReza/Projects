c4bb614de73b4e14cf249014fa876f3e3e5b6445
Ashik044 <rezaashik044@gmail.com>
2024-02-01 22:49:22 +0600

    done

--- src/components/taskHeader.jsx
+++ src/components/taskHeader.jsx
@@ -6,7 +6,7 @@ import TaskAction from "./taskAction.jsx";
 
 export default function TaskHeader() {
   return (
-    <div className="w-full mb-14 items-center justify-between flex-col sm:flex">
+    <div className="w-full mb-14 items-center justify-between sm:flex">
       <h2 className="text-2xl font-semibold max-sm:mb-4 pb-6">Your Tasks</h2>
       <div className="max-sm:flex-col sm:flex justify-center items-center space-x-5">
         <SearchTask />
