88a0d3bbb6bda386bbfd1bef93c14574843b6bf0
Ashik044 <rezaashik044@gmail.com>
2024-02-04 20:49:24 +0600

    done

--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import styles from "../TimerDisplay.module.css";
 
 const TimerDisplay = ({ minutes, seconds }) => {
@@ -24,68 +24,25 @@ const TimerDisplay = ({ minutes, seconds }) => {
     <div className={styles.container}>
       <div className={styles.containerSegment}>
         <div className={styles.segment}>
-          <FlipCard
-            top={flipTimer[0]}
-            bottom={flipTimer[0]}
-            dataKey="minutes-tens"
-          />
-          <FlipCard
-            top={flipTimer[1]}
-            bottom={flipTimer[1]}
-            dataKey="minutes-ones"
-          />
+          <FlipCard value={flipTimer[0]} dataKey="minutes-tens" />
+          <FlipCard value={flipTimer[1]} dataKey="minutes-ones" />
         </div>
       </div>
+
       <div className={styles.middlePointer}>:</div>
+
       <div className={styles.containerSegment}>
         <div className={styles.segment}>
-          <FlipCard
-            top={flipTimer[2]}
-            bottom={flipTimer[2]}
-            dataKey="seconds-tens"
-          />
-          <FlipCard
-            top={flipTimer[3]}
-            bottom={flipTimer[3]}
-            dataKey="seconds-ones"
-          />
+          <FlipCard value={flipTimer[2]} dataKey="seconds-tens" />
+          <FlipCard value={flipTimer[3]} dataKey="seconds-ones" />
         </div>
       </div>
     </div>
   );
 };
 
-const FlipCard = ({ top, bottom, dataKey }) => {
+const FlipCard = ({ value, dataKey }) => {
   const topRef = useRef(null);
-
-  useEffect(() => {
-    if (topRef.current) {
-      const topHalf = topRef.current.querySelector(".top");
-      const bottomHalf = topRef.current.querySelector(".bottom");
-
-      const topFlip = document.createElement("div");
-      topFlip.classList.add(`${styles.topFlip}`); // Use the CSS module class
-      const bottomFlip = document.createElement("div");
-      bottomFlip.classList.add(`${styles.bottomFlip}`); // Use the CSS module class
-
-      topFlip.textContent = top;
-      bottomFlip.textContent = top;
-
-      topFlip.addEventListener("animationstart", () => {
-        topHalf.textContent = "";
-      });
-      topFlip.addEventListener("animationend", () => {
-        topHalf.textContent = bottom;
-        topFlip.remove();
-      });
-      bottomFlip.addEventListener("animationend", () => {
-        bottomHalf.textContent = bottom;
-        bottomFlip.remove();
-      });
-
-      topRef.current.append(topFlip, bottomFlip);
-    }
-  }, [top, bottom]);
 
   return (
     <div
@@ -94,8 +51,8 @@ const FlipCard = ({ top, bottom, dataKey }) => {
       ref={topRef}
       style={{ fontFamily: "Protest Riot, sans-serif" }}
     >
-      <div className={styles.top}>{top}</div>
-      <div className={styles.bottom}>{bottom}</div>
+      <div className={styles.top}>{value}</div>
+      <div className={styles.bottom}>{value}</div>
     </div>
   );
 };
