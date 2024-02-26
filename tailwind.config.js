d925cac7d5c6aa6c64a6936415ff38e3af8b688e
Ashik044 <rezaashik044@gmail.com>
2024-02-26 22:43:53 +0600

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundColor: {
-        "pomodoro-bg": "#fc3030b5",
-        "shortBreak-bg": "#1C92FFB0",
-        "longBreak-bg": "#8FA6AC",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
